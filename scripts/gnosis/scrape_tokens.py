60e128638cbb17c55991028aa17df8daf9620f61
jcalbrecht <jcalbrecht85@gmail.com>
2022-01-24 00:32:13 -0600

    rewrite scrape func to adhere to csv format, remove external api, add cool bar/table

---
+++ scripts/gnosis/scrape_tokens.py
@@ -0,0 +1,62 @@
+from brownie import config, interface
+import os
+import pandas as pd
+from helpers.addresses import registry
+from decimal import Decimal
+from rich.console import Console
+from rich.json import JSON
+from rich.progress import Progress
+from rich.table import Table
+
+
+console = Console()
+
+
+def main():
+    """
+    generate a csv airdrop of all tokens and their balances from `treasurey_tokens` for a given address
+    csv saved to scripts/gnosos/results/<address>_token_balances.csv
+    
+    `scrape` can be called directly from command line with an address:
+    brownie run scrape_tokens.py scrape <address> <receiver>
+    """
+    
+    wallet_address = ''
+    receiver=''
+    scrape(wallet_address, receiver)
+
+def scrape(address, receiver=''):
+    
+    token_data = {'token_type': [], 'token_address': [], 'receiver': [], 'value': [], 'id': [], 'decimals': []}
+    
+    table = Table()
+    table.add_column("token_type", justify="right", style="bright_yellow")
+    table.add_column("token_address", justify="right", style="bright_yellow")
+    table.add_column("receiver", justify="right", style="bright_yellow")
+    table.add_column("value", justify="right", style="bright_yellow")
+    table.add_column("id", justify="right", style="bright_yellow")
+    table.add_column("decimals", justify="right", style="bright_yellow")
+    
+    with Progress() as progress:
+        scraping = progress.add_task("[yellow]Scraping...", total=len(registry.eth.treasury_tokens))
+        for token_name in registry.eth.treasury_tokens:
+            token = interface.ERC20(registry.eth.treasury_tokens[token_name])
+            decimals = token.decimals()
+            bal = token.balanceOf(address)
+            bal_ether = bal / Decimal(10 ** decimals)
+            
+            if bal > 0:
+                token_data['token_type'].append('erc20')
+                token_data['token_address'].append(token.address)
+                token_data['receiver'].append(receiver)
+                token_data['value'].append(bal_ether)
+                token_data['id'].append('')
+                # required if not part of uniswap token list - https://github.com/bh2smith/safe-airdrop#loading-the-app-in-gnosis-safe-interface
+                token_data['decimals'].append(decimals)
+                
+                table.add_row("erc20", token.name(), receiver, str(bal_ether), '', str(decimals))
+                
+            progress.update(scraping, advance=1)
+        
+    console.print(table)
+    pd.DataFrame(token_data).to_csv(f'{os.getcwd()}/scripts/gnosis/results/{address}_token_balances.csv', index=False)
