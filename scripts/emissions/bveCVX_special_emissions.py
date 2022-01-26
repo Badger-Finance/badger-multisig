bed499154fb501bb24a266ca35d4347ea9f5b811
BenLeibig <ben.leibig@gmail.com>
2022-01-26 20:17:13 +0100

    bveCVX yield redirection 26.01.2022

---
+++ scripts/emissions/bveCVX_special_emissions.py
@@ -0,0 +1,29 @@
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import registry
+
+DEVELOPMENT = False
+
+
+
+# UPDATE THESE VARIABLES WITH THE NEEDS FOR A NEW GUARD LAUNCH OR SIMILAR SCENARIO
+STARTING_TIME = 1643241600
+ENDING_TIME = 1644534000
+AMOUNT_TO_EMIT_DURING_PERIOD = 100
+DURATION = ENDING_TIME - STARTING_TIME
+
+
+def main():
+    safe = GreatApeSafe(registry.eth.badger_wallets.dev_multisig)
+    safe.init_badger()
+    rewards_logger = registry.eth.rewardsLogger
+
+    rewards_logger.setUnlockSchedule(
+        registry.eth.treasury_tokens.bveCVX,#beneficiary,
+        registry.eth.treasury_tokens.bveCVX, #payout token
+        Wei(f"{AMOUNT_TO_EMIT_DURING_PERIOD} ether"),
+        STARTING_TIME,
+        ENDING_TIME,
+        DURATION,
+    )
+
+    safe.post_safe_tx()
