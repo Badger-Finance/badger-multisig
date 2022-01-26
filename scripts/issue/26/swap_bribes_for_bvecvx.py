4830a133359d95ca9db5396b4a051fa13f989fe9
jorijnsmit <mail@gosuto.ai>
2022-01-26 22:40:03 +0000

    add locking func

--- scripts/issue/26/swap_bribes_for_bvecvx.py
+++ scripts/issue/26/swap_bribes_for_bvecvx.py
@@ -10,14 +10,18 @@ from helpers.addresses import registry
 
 SAFE = GreatApeSafe(registry.eth.badger_wallets.politician_multisig)
 CVX = SAFE.contract(registry.eth.treasury_tokens.CVX)
-BVECVX = SAFE.contract(registry.eth.treasury_tokens.bveCVX)
+BVECVX = interface.ISettV4h(
+    registry.eth.treasury_tokens.bveCVX, owner=SAFE.account
+)
+TREE = GreatApeSafe(registry.eth.badger_wallets.badgertree)
+TECHOPS = GreatApeSafe(registry.eth.badger_wallets.techops_multisig)
 
 
 def multi_approve():
     SAFE.init_cow()
     for _, addr in registry.eth.bribe_tokens_claimable.items():
         token = SAFE.contract(addr)
-        if token.balanceOf(SAFE) > 1: #1e18:
+        if token.balanceOf(SAFE) > 1_000e18:
             token.approve(SAFE.cow.vault_relayer, token.balanceOf(SAFE))
     SAFE.post_safe_tx(call_trace=True)
 
@@ -26,8 +30,10 @@ def multi_sell():
     SAFE.init_cow()
     for _, addr in registry.eth.bribe_tokens_claimable.items():
         token = SAFE.contract(addr)
-        if token.balanceOf(SAFE) > 1e18:
-            SAFE.cow.market_sell(token, CVX, token.balanceOf(SAFE))
+        if token.balanceOf(SAFE) > 1_000e18:
+            SAFE.cow.market_sell(
+                token, CVX, token.balanceOf(SAFE), deadline=60*60*2
+            )
     SAFE.post_safe_tx()
 
 
@@ -43,9 +49,22 @@ def limit_sell_spell():
 
 
 def swap_bvecvxcvxf_pool():
-    SAFE.make_safesnap(tokens=[CVX.address, BVECVX.address] \
+    SAFE.take_snapshot(tokens=[CVX.address, BVECVX.address] \
         + list(registry['eth']['bribe_tokens_claimable'].values())
     )
     SAFE.init_curve()
     SAFE.curve.swap(CVX, BVECVX, CVX.balanceOf(SAFE))
     SAFE.post_safe_tx()
+
+
+def lock_cvx():
+    TECHOPS.take_snapshot(tokens=[CVX.address, BVECVX.address])
+    TREE.take_snapshot(tokens=[CVX.address, BVECVX.address])
+
+    CVX.approve(BVECVX, CVX.balanceOf(TECHOPS), {'from': TECHOPS.address})
+    BVECVX.depositFor(TREE, CVX.balanceOf(TECHOPS), {'from': TECHOPS.address})
+
+    TREE.print_snapshot()
+    TECHOPS.print_snapshot()
+
+    TECHOPS.post_safe_tx()
