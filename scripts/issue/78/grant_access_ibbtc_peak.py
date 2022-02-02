fce8a8c7f69b29853bf86e6e2e6e52311515ef96
sajanrajdev <sajanrajdev10@gmail.com>
2022-02-02 12:48:16 -0600

    feat: split atomic tx

--- scripts/issue/78/grant_access_ibbtc_peak.py
+++ scripts/issue/78/grant_access_ibbtc_peak.py
@@ -4,16 +4,17 @@ from brownie import interface
 
 TREASURY_OPS = registry.eth.badger_wallets.treasury_ops_multisig
 
-def main():
+def main(peak="badger"):
     safe = GreatApeSafe(registry.ibbtc.dfdBadgerShared)
     safe.init_badger()
 
-    badger_peak = interface.IPeak(registry.ibbtc.badgerPeak, owner=safe.account)
-    byvWbtc_peak = interface.IPeak(registry.ibbtc.byvWbtcPeak, owner=safe.account)
-
-    badger_peak.approveContractAccess(TREASURY_OPS)
-    assert badger_peak.approved(TREASURY_OPS)
-    byvWbtc_peak.approveContractAccess(TREASURY_OPS)
-    assert byvWbtc_peak.approved(TREASURY_OPS)
+    if peak == "badger":
+        badger_peak = interface.IPeak(registry.ibbtc.badgerPeak, owner=safe.account)
+        badger_peak.approveContractAccess(TREASURY_OPS)
+        assert badger_peak.approved(TREASURY_OPS)
+    else:
+        byvWbtc_peak = interface.IPeak(registry.ibbtc.byvWbtcPeak, owner=safe.account)
+        byvWbtc_peak.approveContractAccess(TREASURY_OPS)
+        assert byvWbtc_peak.approved(TREASURY_OPS)
 
     safe.post_safe_tx()
