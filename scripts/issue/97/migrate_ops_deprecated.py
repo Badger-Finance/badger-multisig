86748dfe6856657f0d6c221d02932561f7cf3653
Petrovska <petrovska@badger.finance>
2022-02-08 22:04:18 +0100

    Adding the owned jersey transfer to recipient

--- scripts/issue/97/migrate_ops_deprecated.py
+++ scripts/issue/97/migrate_ops_deprecated.py
@@ -5,6 +5,9 @@ from brownie import Wei
 # https://opensea.io/assets/0xe1e546e25a5ed890dff8b8d005537c0d373497f8/1
 token_id = 1
 
+# one time-off addr, not adding in book
+recipient = "0xdE0AEf70a7ae324045B7722C903aaaec2ac175F5"
+
 
 def main():
     safe = GreatApeSafe(registry.eth.badger_wallets.ops_multisig_old)
@@ -20,19 +23,32 @@ def main():
     # apparently we got 11 units
     jersey_amount = jersey_nft.balanceOf(safe, token_id)
 
+    recipient_amount = jersey_nft.balanceOf(recipient, token_id)
+
+    # 1 unit send to recipient
+    jersey_nft.safeTransferFrom(
+        safe,
+        recipient,
+        token_id,
+        1,
+        bytes(),
+    )
+
     jersey_nft.safeTransferFrom(
         safe,
         registry.eth.badger_wallets.treasury_vault_multisig,
         token_id,
-        jersey_amount,
+        jersey_amount - 1,
         bytes(),
     )
+
+    assert jersey_nft.balanceOf(recipient, token_id) == recipient_amount + 1
 
     assert (
         jersey_nft.balanceOf(
             registry.eth.badger_wallets.treasury_vault_multisig, token_id
         )
-        == jersey_amount
+        == jersey_amount - 1
     )
 
     safe.print_snapshot()
