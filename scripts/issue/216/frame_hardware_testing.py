440a3c9ed3a5bf75c7bff327ad19329d8a6a4545
jorijnsmit <mail@gosuto.ai>
2022-03-09 18:25:33 +0000

    rely on proper `SafeTx` obj for signing and exec

--- scripts/issue/216/frame_hardware_testing.py
+++ scripts/issue/216/frame_hardware_testing.py
@@ -1,25 +1,18 @@
 from great_ape_safe import GreatApeSafe
-from brownie import interface
+from helpers.addresses import registry
 
 
-def main(
-    safe_address=None,
-    erc20_token=None,
-    destination=None,
-    action_trigger=0,
-    safe_nonce=None,
-):
-    safe = GreatApeSafe(safe_address)
+def post_empty_tx():
+    safe = GreatApeSafe(registry.eth.badger_wallets.test_multisig_v1_3)
+    safe.account.transfer(safe, 0)
+    safe.post_safe_tx()
 
-    erc20_token = interface.IERC20(erc20_token, owner=safe.address)
 
-    erc20_token.transfer(destination, erc20_token.balanceOf(safe))
+def sign_with_frame(safe_nonce=None):
+    safe = GreatApeSafe(registry.eth.badger_wallets.test_multisig_v1_3)
+    safe.sign_with_frame_hardware_wallet(safe_nonce)
 
-    safe_nonce = int(safe_nonce) if safe_nonce is not None else None
 
-    if int(action_trigger) == 0:
-        safe.post_safe_tx()
-    elif int(action_trigger) == 1:
-        safe.sign_with_frame_hardware_wallet(safe_nonce)
-    else:
-        safe.execute_with_frame_hardware_wallet(safe_nonce)
+def exec_with_frame(safe_nonce=None):
+    safe = GreatApeSafe(registry.eth.badger_wallets.test_multisig_v1_3)
+    safe.execute_with_frame_hardware_wallet(safe_nonce)
