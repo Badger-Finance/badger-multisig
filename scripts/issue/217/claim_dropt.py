03f662362798c76e1b7ad834e2d34d033ec7232d
jcalbrecht <jcalbrecht85@gmail.com>
2022-03-09 11:42:56 -0600

    add claimble tokens to snapshot

--- scripts/issue/217/claim_dropt.py
+++ scripts/issue/217/claim_dropt.py
@@ -11,16 +11,21 @@ def main():
     safe = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
     safe.init_badger()
 
+    # reward tokens
     digg = registry.eth.treasury_tokens.DIGG
+    bcvxCRV = registry.eth.treasury_tokens.bcvxCRV
+    bveCVX = registry.eth.treasury_tokens.bveCVX
+
     bdigg = interface.ISettV4h(registry.eth.treasury_tokens.bDIGG, owner=safe.address)
     dropt = safe.contract(registry.eth.uma.DIGG_LongShortPair)
     dropt_long = safe.contract(dropt.longToken())
 
-
     safe.take_snapshot(tokens=[
         bdigg.address,
         digg,
-        dropt_long
+        dropt_long,
+        bcvxCRV,
+        bveCVX,
     ])
 
     safe.badger.claim_all()
@@ -28,7 +33,7 @@ def main():
     dropt_long_bal = dropt_long.balanceOf(safe)
     # redeem dropt-3 for bdigg
     dropt.settle(dropt_long_bal, 0)
-    
+
     bdigg.withdrawAll()
 
     safe.print_snapshot()
