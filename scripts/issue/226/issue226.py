21e0531c4930e1addffce187944f277f0c0fa044
jcalbrecht <jcalbrecht85@gmail.com>
2022-03-09 13:44:01 -0600

    try to add fbveCVX...

---
+++ scripts/issue/226/issue226.py
@@ -0,0 +1,18 @@
+from sympy import E
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import registry
+from brownie import interface
+
+
+COLLATERAL_FACTOR = 0.5
+
+
+def main():
+    safe = GreatApeSafe(registry.eth.badger_wallets.dev_multisig)
+    safe.init_rari()
+
+    fbveCVX = interface.IFToken(registry.eth.rari['fbveCVX-22'])
+    safe.rari.add_ftoken_to_pool(fbveCVX)
+    safe.rari.ftoken_pause(fbveCVX)
+
+    safe.post_safe_tx()
