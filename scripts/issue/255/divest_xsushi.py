7a5039efaa7bfb5edd46d6ce37c55fb06d5af724
jorijnsmit <mail@gosuto.ai>
2022-03-17 08:28:52 +0000

    add market and ladder orders for cowswap, add xsushi interface

--- scripts/issue/255/divest_xsushi.py
+++ scripts/issue/255/divest_xsushi.py
@@ -1,12 +1,13 @@
+from brownie import interface
+
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
-from brownie import Contract, interface
 
 
 VAULT = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
 TROPS = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
 
-xSUSHI = Contract(registry.eth.treasury_tokens.xSUSHI)
+xSUSHI = interface.ISushiBar(registry.eth.treasury_tokens.xSUSHI)
 SUSHI = TROPS.contract(registry.eth.treasury_tokens.SUSHI)
 CVX = TROPS.contract(registry.eth.treasury_tokens.CVX)
 BVECVX = interface.ISettV4h(
@@ -28,11 +29,19 @@ def unwrap_xsushi():
     TROPS.post_safe_tx()
 
 
-def market_sell_sushi(amount_eth):
+def post_orders_to_cowswap():
     TROPS.init_cow()
-    amount = amount_eth * 10 ** SUSHI.decimals()
-    SUSHI.approve(TROPS, amount)
-    TROPS.cow.market_sell(SUSHI, CVX, amount)
+    TROPS.cow.allow_relayer(SUSHI, SUSHI.balanceOf(TROPS))
+    half = int(SUSHI.balanceOf(TROPS) / 2)
+    a_tenth = int(SUSHI.balanceOf(TROPS) / 10)
+    rest = SUSHI.balanceOf(TROPS) - half - 4 * a_tenth
+    assert half + 4 * a_tenth + rest == SUSHI.balanceOf(TROPS)
+    TROPS.cow.market_sell(SUSHI, CVX, half, deadline=60*60*24)
+    TROPS.cow.market_sell(SUSHI, CVX, a_tenth, deadline=60*60*24*10, coef=.99)
+    TROPS.cow.market_sell(SUSHI, CVX, a_tenth, deadline=60*60*24*10, coef=.98)
+    TROPS.cow.market_sell(SUSHI, CVX, a_tenth, deadline=60*60*24*10, coef=.97)
+    TROPS.cow.market_sell(SUSHI, CVX, a_tenth, deadline=60*60*24*10, coef=.96)
+    TROPS.cow.market_sell(SUSHI, CVX, rest, deadline=60*60*24*10, coef=.95)
     TROPS.post_safe_tx()
 
 
