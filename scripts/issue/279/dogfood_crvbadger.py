fa750c3e2851a0b3054e27f52d3a0ab38aff686b
jorijnsmit <mail@gosuto.ai>
2022-03-22 09:45:53 +0100

    initial draft to dogfood crvbadger position

---
+++ scripts/issue/279/dogfood_crvbadger.py
@@ -0,0 +1,27 @@
+from brownie import Contract, interface
+
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import registry
+
+
+def main():
+    safe = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
+    vault = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
+
+    crvbadger = Contract(
+        registry.eth.treasury_tokens.badgerWBTC_f, owner=safe.account
+    )
+    bcrvbadger = interface.ISettV4h(
+        registry.eth.sett_vaults.bcrvBadger, owner=safe.account
+    )
+
+    # bcrvbadger.approveContractAccess(safe, {'from': bcrvbadger.governance()})
+
+    vault.take_snapshot([crvbadger, bcrvbadger])
+    safe.take_snapshot([crvbadger, bcrvbadger])
+    bal_crvbadger = crvbadger.balanceOf(safe)
+    crvbadger.approve(bcrvbadger, bal_crvbadger)
+    bcrvbadger.depositFor(vault, bal_crvbadger)
+    vault.print_snapshot()
+    safe.print_snapshot()
+    safe.post_safe_tx(call_trace=True)
