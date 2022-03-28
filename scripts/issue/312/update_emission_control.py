9d445b0f51a9f5a26e8c007dcf8c51946d95773a
mitche50 <mitche50@gmail.com>
2022-03-28 10:13:24 -0400

    chore: use correct multisig and post

--- scripts/issue/312/update_emission_control.py
+++ scripts/issue/312/update_emission_control.py
@@ -1,4 +1,4 @@
-from brownie import interface
+from brownie import interface, Contract
 
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
@@ -6,7 +6,7 @@ from helpers.addresses import registry
 FINAL_TOKEN_WEIGHT = 5000
 
 def main():
-    safe = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
+    safe = GreatApeSafe(registry.eth.badger_wallets.techops_multisig)
     emission_control = interface.IEmissionControl(registry.eth.EmissionControl, owner=safe.account)
 
     # Check inital token weight to ensure upgrade hasn't already happened
@@ -17,3 +17,5 @@ def main():
 
     updated_token_weight = emission_control.tokenWeight(registry.eth.sett_vaults.bcrvBadger)
     assert updated_token_weight == FINAL_TOKEN_WEIGHT, f"Incorrect final token weight set: {updated_token_weight}"
+
+    safe.post_safe_tx()
