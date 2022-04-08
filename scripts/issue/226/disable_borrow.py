5e18a59b5f4eecc9fa8b7c58175834638b3bbe39
jorijnsmit <mail@gosuto.ai>
2022-04-08 19:15:57 +0200

    `ftoken_set_admin_fee` requires ftoken address

--- scripts/issue/226/disable_borrow.py
+++ scripts/issue/226/disable_borrow.py
@@ -8,10 +8,10 @@ from eth_abi import encode_abi
 def main(ftoken_addr):
     safe = GreatApeSafe(registry.eth.badger_wallets.dev_multisig)
     safe.init_rari()
-    
+
     safe.rari.ftoken_pause(ftoken_addr, rf=0)
 
     if safe.rari.ftoken_get_admin_fee != 0:
-        safe.rari.ftoken_set_admin_fee(0)
+        safe.rari.ftoken_set_admin_fee(ftoken_addr, 0)
 
     safe.post_safe_tx()
