8fca559aefc38c3e4edc042ed9d21f16ca731cbf
jorijnsmit <mail@gosuto.ai>
2022-04-08 18:55:29 +0200

    remove unnecessary declarations now that super func is used for release

--- contracts/RemBadgerDripper.sol
+++ contracts/RemBadgerDripper.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/finance/VestingWallet.sol";
 
 contract RemBadgerDripper is VestingWallet {
-    mapping(address => uint256) private _erc20Released;
     address public constant controller = 0x86cbD0ce0c087b482782c181dA8d191De18C8275;
     address public constant governance = 0xB65cef03b9B89f99517643226d76e286ee999e77;
     address private _keeper;
@@ -20,12 +19,9 @@ contract RemBadgerDripper is VestingWallet {
         startTimestamp,
         durationSeconds
     ) {
-        _controller = 0x86cbD0ce0c087b482782c181dA8d191De18C8275; //techops
-        _governance = 0xB65cef03b9B89f99517643226d76e286ee999e77; //dev
         require(keeperAddress != address(0), "RemBadgerDripper: keeper is zero address");
         _keeper = keeperAddress;
     }
-
 
     /**
      * @dev Getter for the keeper address.
@@ -39,19 +35,15 @@ contract RemBadgerDripper is VestingWallet {
      */
     function setKeeper(address _newKeeper) public {
         require(
-            _msgSender() == controller() || _msgSender() == governance(),
+            _msgSender() == controller || _msgSender() == governance,
             "RemBadgerDripper: onlyAuthorized"
         );
         _keeper = _newKeeper;
     }
 
-    function released(address token) public view virtual override returns (uint256) {
-        return _erc20Released[token];
-    }
-
     function release(address token) public virtual override {
         require(
-            _msgSender() == keeper() || _msgSender() == controller() || _msgSender() == governance(),
+            _msgSender() == keeper() || _msgSender() == controller || _msgSender() == governance,
             "RemBadgerDripper: onlyAuthorized"
         );
         VestingWallet.release(token);
