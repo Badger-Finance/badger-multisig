8d6ffd71087772ea7d53d0db2126e9a487bc2c48
jorijnsmit <mail@gosuto.ai>
2022-04-12 08:20:24 +0200

    give every child of UniV2 its private constructor. constants are shared

--- great_ape_safe/ape_api/spookyswap.py
+++ great_ape_safe/ape_api/spookyswap.py
@@ -4,9 +4,8 @@ from helpers.addresses import registry
 
 class SpookySwap(UniV2):
     def __init__(self, safe):
-        super().__init__(safe)
+        self.safe = safe
+        self.router = self.safe.contract(registry.ftm.spookyswap.router)
+        self.factory = self.safe.contract(registry.ftm.spookyswap.factory)
 
-        self.router = safe.contract(registry.ftm.spookyswap.router)
-        self.factory = safe.contract(registry.ftm.spookyswap.factory)
-
-        self.max_slippage = 0.05
+    max_slippage = 0.05
