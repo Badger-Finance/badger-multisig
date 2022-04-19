f23693d4eb3c50be5c69f747973e114722461536
jcalbrecht <jcalbrecht85@gmail.com>
2022-04-19 15:48:54 -0500

    swap eps for bnb script

---
+++ scripts/issue/352/swap_eps.py
@@ -0,0 +1,17 @@
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import registry
+
+
+def main():
+
+    safe = GreatApeSafe(registry.bsc.badger_wallets.dev_multisig)
+    eps = safe.contract(registry.bsc.treasury_tokens.EPS)
+    wbnb = safe.contract(registry.bsc.treasury_tokens.WBNB)
+
+    safe.take_snapshot([eps])
+
+    safe.init_pancakeswap_v2()
+    safe.pancakeswap_v2.swap_exact_tokens_for_eth(eps, eps.balanceOf(safe), [eps, wbnb])
+
+    safe.print_snapshot()
+    safe.post_safe_tx()
