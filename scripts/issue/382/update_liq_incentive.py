76d6ca1bf6681a023fde704e51c1fcf869e31d3f
jcalbrecht <jcalbrecht85@gmail.com>
2022-04-25 16:06:47 -0500

    update liq incentive through cli

--- scripts/issue/382/update_liq_incentive.py
+++ scripts/issue/382/update_liq_incentive.py
@@ -2,14 +2,12 @@ from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
 
 
-LIQUIDATION_INCENTIVE = 1.12
-
-
-def main():
+def main(new_incentive):
+    # ex 10% incentive: new_incentive = '1.10'
 
     safe = GreatApeSafe(registry.eth.badger_wallets.dev_multisig)
     safe.init_rari()
 
-    safe.rari.set_liquidation_incentive(LIQUIDATION_INCENTIVE)
+    safe.rari.set_liquidation_incentive(float(new_incentive))
 
     safe.post_safe_tx(events=True)
