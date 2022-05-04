2efd96aff97ba4e70310b6cefdadd23759e244f4
jorijnsmit <mail@gosuto.ai>
2022-05-04 19:32:45 +0200

    fix: remove old copypasta

--- scripts/issue/311/month_5_csvless.py
+++ scripts/issue/311/month_5_csvless.py
@@ -1,36 +1,4 @@
-
-import pandas as pd
 from decimal import Decimal
-
-from helpers.addresses import registry
-
-
-def main():
-    df = {"token_address": [], "receiver": [], "value": []}
-
-    # https://github.com/Badger-Finance/badger-multisig/issues/311#issuecomment-1108524915
-    df["token_address"].append(registry.eth.treasury_tokens.BADGER)
-    df["receiver"].append(registry.eth.badger_wallets.treasury_ops_multisig)
-    df["value"].append(Decimal('150_000'))
-
-    df["token_address"].append(registry.eth.treasury_tokens.BADGER)
-    df["receiver"].append(registry.eth.drippers.rembadger_2022_q2)
-    df["value"].append(Decimal('51_913.076921'))
-
-    df = pd.DataFrame(df)
-    df["token_type"] = "erc20"
-    df["id"] = pd.NA
-
-    # build dataframe for airdrop and dump to csv
-    airdrop = df[["token_type", "token_address", "receiver", "value", "id"]]
-    airdrop.to_csv(
-        "scripts/issue/311/month_5.csv",
-        index=False,
-        header=["token_type", "token_address", "receiver", "value", "id"],
-    )
-    print(airdrop)
-
-
 
 from brownie import interface
 
@@ -51,6 +19,7 @@ def main():
     tree.take_snapshot([badger])
     dripper.take_snapshot([badger])
 
+    # https://github.com/Badger-Finance/badger-multisig/issues/311#issuecomment-1108524915
     badger.transfer(tree, 150_000e18)
     badger.transfer(dripper, Decimal(51_913.076921) * Decimal(1e18))
 
