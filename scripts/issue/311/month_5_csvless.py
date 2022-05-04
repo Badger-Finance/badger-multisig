abd94c28e0c9f3bffb7085d2f10a26a3a650e0ee
jorijnsmit <mail@gosuto.ai>
2022-05-04 19:34:04 +0200

    fix: quarterly topup should go to trops, not tree

--- scripts/issue/311/month_5_csvless.py
+++ scripts/issue/311/month_5_csvless.py
@@ -8,7 +8,7 @@ from helpers.addresses import registry
 
 def main():
     safe = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
-    tree = GreatApeSafe(registry.eth.badger_wallets.badgertree)
+    trops = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
     dripper = GreatApeSafe(registry.eth.drippers.rembadger_2022_q2)
 
     badger = interface.ERC20(
@@ -16,15 +16,15 @@ def main():
     )
 
     safe.take_snapshot([badger])
-    tree.take_snapshot([badger])
+    trops.take_snapshot([badger])
     dripper.take_snapshot([badger])
 
     # https://github.com/Badger-Finance/badger-multisig/issues/311#issuecomment-1108524915
-    badger.transfer(tree, 150_000e18)
+    badger.transfer(trops, 150_000e18)
     badger.transfer(dripper, Decimal(51_913.076921) * Decimal(1e18))
 
     safe.print_snapshot()
-    tree.print_snapshot()
+    trops.print_snapshot()
     dripper.print_snapshot()
 
     safe.post_safe_tx()
