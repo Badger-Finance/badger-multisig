3156fbf7dc192bdc2b51b49724d91e37acc633ad
jcalbrecht <jcalbrecht85@gmail.com>
2022-05-04 22:41:47 -0500

    week18 topup script

---
+++ scripts/badger/topups/week_18.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+
+from brownie import interface
+
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import registry
+
+
+def main():
+    safe = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
+    tree = GreatApeSafe(registry.eth.badger_wallets.badgertree)
+
+    badger = interface.ERC20(
+        registry.eth.treasury_tokens.BADGER, owner=safe.account
+    )
+
+    digg = interface.ERC20(
+        registry.eth.treasury_tokens.DIGG, owner=safe.account
+    )
+
+    safe.take_snapshot([badger, digg])
+    tree.take_snapshot([badger, digg])
+
+    # badger emissions
+    week_18_badger_emissions = Decimal(20_000e18)
+    week_18_rembadger_emissions = Decimal(7692.307692e18)
+
+    badger.transfer(
+        tree, week_18_badger_emissions + week_18_rembadger_emissions
+    )
+
+    # digg emissions
+    week_18_digg_emissions_tree = Decimal(1.302461219e9)
+    digg.transfer(tree, week_18_digg_emissions_tree)
+
+    safe.print_snapshot()
+    tree.print_snapshot()
