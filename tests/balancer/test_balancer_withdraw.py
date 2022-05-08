7081038362ed28563de2f034aeb15c34c9d918de
jcalbrecht <jcalbrecht85@gmail.com>
2022-05-08 18:14:49 -0500

    add more withdraw tests

--- tests/balancer/test_balancer_withdraw.py
+++ tests/balancer/test_balancer_withdraw.py
@@ -1,8 +1,9 @@
 import pytest
+from brownie import chain
 
 
-@pytest.fixture(autouse=True)
-def deposited(dev, balancer, wbtc, weth, staked_bpt):
+@pytest.fixture()
+def deposited_weighted(dev, balancer, wbtc, weth, staked_bpt):
     bal_before_staked_bpt = staked_bpt.balanceOf(dev)
     assert bal_before_staked_bpt == 0
 
@@ -16,10 +17,24 @@ def deposited(dev, balancer, wbtc, weth, staked_bpt):
     assert staked_bpt.balanceOf(dev) > 0
 
 
-def test_unstake_and_withdraw_all(dev, balancer, wbtc, weth, bpt, staked_bpt):
+@pytest.fixture()
+def deposited_stable(dev, balancer, dai, threepool_bpt, threepool_staked_bpt):
+    bal_before_staked_bpt = threepool_staked_bpt.balanceOf(dev)
+    bal_before_dai = dai.balanceOf(dev)
+
+    balancer.deposit_and_stake_single_asset(
+        dai,
+        dai.balanceOf(dev) / 4,
+        threepool_bpt
+    )
+
+    assert dai.balanceOf(dev) < bal_before_dai
+    assert threepool_staked_bpt.balanceOf(dev) > bal_before_staked_bpt
+
+
+def test_unstake_and_withdraw_all_weighted(dev, balancer, wbtc, weth, bpt, staked_bpt, deposited_weighted):
     bal_before_weth = weth.balanceOf(dev)
     bal_before_wbtc = wbtc.balanceOf(dev)
-    bal_before_bpt = bpt.balanceOf(dev)
 
     balancer.unstake_all_and_withdraw_all(pool=bpt)
 
@@ -27,16 +42,24 @@ def test_unstake_and_withdraw_all(dev, balancer, wbtc, weth, bpt, staked_bpt):
     assert wbtc.balanceOf(dev) > bal_before_wbtc
 
     assert staked_bpt.balanceOf(dev) == 0
-    assert bpt.balanceOf(dev) < bal_before_bpt
+    assert bpt.balanceOf(dev) == 0
 
 
-def test_unstake_and_withdraw_all_single_asset(dev, balancer, wbtc, bpt, staked_bpt):
+def test_unstake_and_withdraw_all_single_asset(dev, balancer, wbtc, bpt, staked_bpt, deposited_weighted):
     bal_before_wbtc = wbtc.balanceOf(dev)
-    bal_before_bpt = bpt.balanceOf(dev)
 
     balancer.unstake_and_withdraw_all_single_asset(wbtc, pool=bpt)
 
     assert wbtc.balanceOf(dev) > bal_before_wbtc
-
     assert staked_bpt.balanceOf(dev) == 0
-    assert bpt.balanceOf(dev) < bal_before_bpt
+    assert bpt.balanceOf(dev) == 0
+
+
+def test_unstake_and_withdraw_all_stable(dev, balancer, dai, threepool_bpt, threepool_staked_bpt):
+    bal_before_dai = dai.balanceOf(dev)
+
+    balancer.unstake_and_withdraw_all_single_asset(dai, pool=threepool_bpt)
+
+    assert dai.balanceOf(dev) > bal_before_dai
+    assert threepool_staked_bpt.balanceOf(dev) == 0
+    assert threepool_bpt.balanceOf(dev) == 0
