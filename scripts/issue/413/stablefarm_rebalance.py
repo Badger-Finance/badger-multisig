75d276520b462f1fa377d7bcfea1eb0719899de9
jcalbrecht <jcalbrecht85@gmail.com>
2022-05-12 13:04:25 -0500

    calc correct usdc

--- scripts/issue/413/stablefarm_rebalance.py
+++ scripts/issue/413/stablefarm_rebalance.py
@@ -2,8 +2,20 @@ from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
 
 
-def main():
+def send_3pool_to_vault():
     trops = GreatApeSafe(registry.eth.badger_wallets.treasury_ops_multisig)
+    vault = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
+    crv3pool = trops.contract(registry.eth.treasury_tokens.crv3pool)
+
+    trops.take_snapshot(tokens=[crv3pool.address])
+    vault.take_snapshot(tokens=[crv3pool.address])
+
+    crv3pool.transfer(vault, crv3pool.balanceOf(trops))
+
+    trops.post_safe_tx()
+
+
+def main():
     vault = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
     vault.init_curve()
     vault.init_convex()
@@ -17,7 +29,7 @@ def main():
     fei = vault.contract(registry.eth.treasury_tokens.FEI)
     eurs = vault.contract(registry.eth.treasury_tokens.EURS)
 
-    crv3pool = trops.contract(registry.eth.treasury_tokens.crv3pool)
+    crv3pool = vault.contract(registry.eth.treasury_tokens.crv3pool)
     crv3eur = vault.contract(registry.eth.treasury_tokens.crv3eur)
     crvfrax = vault.contract(registry.eth.treasury_tokens.crvFRAX)
 
@@ -30,7 +42,6 @@ def main():
     bpt3pool = vault.contract(registry.eth.balancer.B_3POOL)
     bpt3pool_gauge = vault.balancer.gauge_factory.getPoolGauge(bpt3pool)
 
-    trops.take_snapshot(tokens=[crv3pool.address])
     vault.take_snapshot(tokens=[
         crv3pool.address,
         ausdc.address,
@@ -42,24 +53,20 @@ def main():
     ])
 
     # withdrawals
-    crv3pool.transfer(vault, crv3pool.balanceOf(trops))
-
     vault.convex.unstake_all_and_withdraw_all(crv3pool)
     vault.convex.unstake_all_and_withdraw_all(crv3eur)
     vault.convex.unstake_all_and_withdraw_all(crvfrax)
 
-
     vault.curve.withdraw_to_one_coin(crv3eur, crv3eur.balanceOf(vault), eurs)
-
-    vault.curve.swap(eurs, usdc, eurs.balanceOf(vault))
     vault.curve.withdraw_to_one_coin(crvfrax, crvfrax.balanceOf(vault), crv3pool)
 
-
-    # vault.curve.withdraw_to_one_coin(crv3pool, 614_746e18, usdc)
     vault.curve.withdraw_to_one_coin(crv3pool, 614_746e18, usdt)
 
     vault.curve.withdraw_to_one_coin(crv3pool, 614_746e18 + 307_373e18, dai)
     vault.curve.swap(dai, fei, 307_373e18, i=1, j=0)
+
+    vault.curve.swap(eurs, usdc, eurs.balanceOf(vault))
+    vault.curve.withdraw_to_one_coin(crv3pool, (614_746e6 - usdc.balanceOf(vault)) * 1e12, usdc)
 
     # deposits
     vault.aave.deposit(usdc, usdc.balanceOf(vault))
@@ -73,6 +80,9 @@ def main():
     amounts = [int(usdt.balanceOf(vault)), int(usdc.balanceOf(vault)), int(dai.balanceOf(vault))]
     vault.balancer.deposit_and_stake(underlyings, amounts)
 
+    # re-deposit remaining crv3pool
+    vault.convex.deposit_all_and_stake(crv3pool)
 
-    # trops.print_snapshot()
     vault.print_snapshot()
+
+    vault.post_safe_tx()
