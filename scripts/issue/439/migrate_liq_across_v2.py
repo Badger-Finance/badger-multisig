fde87adf8deb915e6a9b7f8ef3566b476c03a72e
Petrovska <petrovska@badger.finance>
2022-05-18 16:17:55 +0200

    Isolate sim for ease of running

--- scripts/issue/439/migrate_liq_across_v2.py
+++ scripts/issue/439/migrate_liq_across_v2.py
@@ -1,19 +1,26 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
-from brownie import accounts
+from brownie import accounts, Contract
 
 # not sure how big this figure should be based on the time from posting to signing
 # amount available to wd may vary from others lps migrating
 SAFE_WD_AMOUNT_TILL_SIGN = 10e18
 
 
-def main(sim=False):
+def main(sim="True"):
     safe = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
     a_badger_lp = safe.contract(registry.eth.treasury_tokens.aBADGER)
     badger = safe.contract(registry.eth.treasury_tokens.BADGER)
     hub_pool = safe.contract(registry.eth.across_bridge.hub_pool)
 
     safe.take_snapshot(tokens=[a_badger_lp, badger])
+
+    if sim == "True":
+        # isolating for smoother trace once sim
+        # force sim, in light of lack of badger whitelisting atm
+        owner = accounts.at(hub_pool.owner(), force=True)
+        hub_pool.enableL1TokenForLiquidityProvision(badger.address, {"from": owner})
+        return
 
     exchange_rate = a_badger_lp.exchangeRateCurrent().return_value / 1e18
     liquid_reserves = a_badger_lp.liquidReserves()
@@ -28,11 +35,6 @@ def main(sim=False):
     a_badger_lp.removeLiquidity(lp_withdraw_mantisa, False)
 
     badger.approve(hub_pool, exchange_rate * lp_withdraw_mantisa)
-
-    if sim:
-        # force sim, in light of lack of badger whitelisting atm
-        owner = accounts.at(hub_pool.owner(), force=True)
-        hub_pool.enableL1TokenForLiquidityProvision(badger.address, {"from": owner})
 
     hub_pool.addLiquidity(badger.address, exchange_rate * lp_withdraw_mantisa)
 
