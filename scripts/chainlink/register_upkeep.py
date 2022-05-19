70aa4873a71fa495ad3b4fe3d90065ff16f55748
jorijnsmit <mail@gosuto.ai>
2022-05-19 16:46:44 +0200

    chore: remove comments; confirmed args can remain null

--- scripts/chainlink/register_upkeep.py
+++ scripts/chainlink/register_upkeep.py
@@ -8,7 +8,7 @@ from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
 
 
-LINK_MANTISSA = 5e18
+LINK_MANTISSA = 125e18
 
 
 def main():
@@ -20,15 +20,19 @@ def main():
         registry.eth.treasury_tokens.LINK, owner=safe.account
     )
 
+    safe.take_snapshot([link])
+
     data = relayer.register.encode_input(
         'TreeDripper2022Q2', # string memory name,
-        b'', # TODO # bytes calldata encryptedEmail,
+        b'', # bytes calldata encryptedEmail,
         registry.eth.drippers.tree_2022_q2, # address upkeepContract,
         400_000, # uint32 gasLimit,
         safe.address, # address adminAddress,
         b'', # bytes calldata checkData,
         LINK_MANTISSA, # uint96 amount,
-        0 # TODO # uint8 source
+        0 # uint8 source
     )
 
     link.transferAndCall(relayer, LINK_MANTISSA, data)
+
+    safe.post_safe_tx(call_trace=True)
