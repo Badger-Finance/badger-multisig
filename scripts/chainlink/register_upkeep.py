bc4ed08091d55407c176e52096ab5e8937d3e624
jorijnsmit <mail@gosuto.ai>
2022-05-26 16:02:33 +0200

    fix: keep old script intact

--- scripts/chainlink/register_upkeep.py
+++ scripts/chainlink/register_upkeep.py
@@ -13,15 +13,26 @@ LINK_MANTISSA = 125e18
 
 def main():
     safe = GreatApeSafe(registry.eth.badger_wallets.techops_multisig)
-    safe.init_chainlink()
-
-    safe.take_snapshot([safe.chainlink.link])
-
-    safe.chainlink.register_upkeep(
-        'GasStationExact',
-        registry.eth.drippers.tree_2022_q2,
-        400_000,
-        125e18
+    relayer = interface.IUpkeepRegistrationRequests(
+        registry.eth.chainlink.upkeep_registration_requests, owner=safe.account
     )
+    link = interface.ILinkToken(
+        registry.eth.treasury_tokens.LINK, owner=safe.account
+    )
+
+    safe.take_snapshot([link])
+
+    data = relayer.register.encode_input(
+        'TreeDripper2022Q2', # string memory name,
+        b'', # bytes calldata encryptedEmail,
+        registry.eth.drippers.tree_2022_q2, # address upkeepContract,
+        400_000, # uint32 gasLimit,
+        safe.address, # address adminAddress,
+        b'', # bytes calldata checkData,
+        LINK_MANTISSA, # uint96 amount,
+        0 # uint8 source
+    )
+
+    link.transferAndCall(relayer, LINK_MANTISSA, data)
 
     safe.post_safe_tx(call_trace=True)
