367b74d2115e16857dae4b1ba81cb2f138df89e9
jintao <hellojintao@protonmail.com>
2022-05-30 11:17:28 -0400

    chore: update vault state

--- scripts/issue/365/register_vaults.py
+++ scripts/issue/365/register_vaults.py
@@ -17,7 +17,7 @@ FANTOM_VAULTS = [
     '0xB6D63A4e5Ca740e96C26AdAbcAc73BE78Ee39Dc5',
     'v1',
     'name=WBTC/renBTC,protocol=Solidex,behavior=Compounder',
-    2
+    3
   ],
   [
     '0x7cc6049a125388B51c530e51727A87aE101f6417',
@@ -35,25 +35,25 @@ FANTOM_VAULTS = [
     '0xd9770deC6fdA576450e66f0c441B6b8755F7184A',
     'v1',
     'name=WEVE/USDC,protocol=Solidex,behavior=Compounder',
-    2
+    3
   ],
   [
     '0x4145a9B8B7bdC76F14e65c26f9c71e23B9069b25',
     'v1',
     'name=WFTM/CRV,protocol=Solidex,behavior=Bitcoin Rewards',
-    2
+    3
   ],
   [
     '0x1AaF7f8154704D80E2380B3DbC967FD0486016e0',
     'v1',
     'name=USDC/MIM,protocol=Solidex,behavior=Bitcoin Rewards',
-    2
+    3
   ],
   [
     '0x9a28eeFB2c5F8311f37c39314D78Be85C54B5da6',
     'v1',
     'name=WFTM/renBTC,protocol=Solidex,behavior=Bitcoin Rewards',
-    2
+    3
   ],
   [
     '0x02330E37fcc7C27eB4632b3E31b951fDE5126c48',
@@ -95,25 +95,25 @@ FANTOM_VAULTS = [
     '0x96d4dBdc91Bef716eb407e415c9987a9fAfb8906',
     'v1.5',
     'name=bveOXD,protocol=0xDAO,behavior=None',
-    2
+    3
   ],
   [
     '0xa8bD8655A0dCABE76913D821Ab437562276b3B59',
     'v1.5',
     'name=boxSOLID,protocol=0xDAO,behavior=Ecosystem Helper',
-    2
+    3
   ],
   [
     '0xbF2F3a9ba42A00CA5B18842D8eB1954120e4a2A9',
     'v1.5',
     'name=bveOXD/OXD,protocol=0xDAO,behavior=Ecosystem Helper',
-    2
+    3
   ],
   [
     '0xcd1e62b390373fcFeA87Dd06E2497Fdc907935fA',
     'v1.5',
     'name=USDC/DEI,protocol=0xDAO,behavior=Ecosystem',
-    2
+    3
   ]
 ]
 
