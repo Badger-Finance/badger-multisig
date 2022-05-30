7770d074ca3c752e400f9a5c845a2c87ba532eae
Petrovska <petrovska@badger.finance>
2022-05-30 22:10:12 -0100

    Set pause guardian

--- scripts/issue/358/set_guardian_rari_pool.py
+++ scripts/issue/358/set_guardian_rari_pool.py
@@ -7,7 +7,7 @@ def main():
 
     dev_msig.init_rari()
 
-    dev_msig.rari.set_borrow_guardian(registry.eth.guardian)
+    # only set pause guardian
     dev_msig.rari.set_pause_guardian(registry.eth.guardian)
 
     dev_msig.post_safe_tx()
