79d84535a182008c0da5924f4262aef422edd7f9
jcalbrecht <jcalbrecht85@gmail.com>
2022-06-01 10:35:15 -0500

    split up withdrawals

--- scripts/issue/500/rari_withdraw.py
+++ scripts/issue/500/rari_withdraw.py
@@ -1,33 +1,32 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
 from brownie import interface
-from dotmap import DotMap
 
 
-def main():
-    safe = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
+safe = GreatApeSafe(registry.eth.badger_wallets.treasury_vault_multisig)
 
-    fwbtc = interface.IFToken(registry.eth.rari['fWBTC-22'], owner=safe.account)
+
+def withdraw_udsc():
     fusdc = interface.IFToken(registry.eth.rari['fUSDC-22'], owner=safe.account)
 
     amount_fusdc = int(fusdc.balanceOf(safe) * 0.75)
+    tx_data = fusdc.redeem.encode_input(amount_fusdc)
+
+    safe_tx = safe.build_multisig_tx(
+        to=registry.fusdc.address, value=0, data=tx_data, operation=1
+    )
+
+    safe.post_transaction(safe_tx)
+
+
+def withdraw_wbtc():
+    fwbtc = interface.IFToken(registry.eth.rari['fWBTC-22'], owner=safe.account)
+
     amount_fwbtc = int(fwbtc.balanceOf(safe) * 0.75)
-    tx_data_usdc = fusdc.redeem.encode_input(amount_fusdc)
-    tx_data_wbtc = fwbtc.redeem.encode_input(amount_fwbtc)
+    tx_data = fwbtc.redeem.encode_input(amount_fwbtc)
 
-    txs = [
-        DotMap({
-            'receiver': fusdc.address,
-            'value': 0,
-            'input': tx_data_usdc,
-        }),
-        DotMap({
-            'receiver': fwbtc.address,
-            'value': 0,
-            'input': tx_data_wbtc
-        })
-    ]
+    safe_tx = safe.build_multisig_tx(
+        to=registry.fwbtc.address, value=0, data=tx_data, operation=1
+    )
 
-    safe_multi_tx = safe.multisend_from_receipts(txs)
-
-    safe.post_transaction(safe_multi_tx)
+    safe.post_transaction(safe_tx)
