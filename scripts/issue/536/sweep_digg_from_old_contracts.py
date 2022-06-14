f55e8bea5f0bea48e19815c637c9963e03d9a27a
jorijnsmit <mail@gosuto.ai>
2022-06-14 10:19:45 +0200

    feat: sweep all remaining assets in dev to trops

--- scripts/issue/536/sweep_digg_from_old_contracts.py
+++ scripts/issue/536/sweep_digg_from_old_contracts.py
@@ -1,4 +1,4 @@
-from brownie import DummyController, accounts, interface
+from brownie import DummyController, interface
 
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
@@ -12,14 +12,28 @@ def main():
     )
     digg = safe.contract(r.treasury_tokens.DIGG)
     wbtc = safe.contract(r.treasury_tokens.WBTC)
-    dummy = DummyController.deploy({'from': accounts[0]})
+    renbtc = safe.contract(r.treasury_tokens.renBTC)
+    yvwbtc = safe.contract(r.treasury_tokens.yvWBTC)
+    imbtc = safe.contract(r.mstable_vaults.imBTC)
+    fpmbtchbtc = safe.contract(r.mstable_vaults.FpMbtcHbtc)
+    dummy = DummyController.at(r.controllers.dummy)
 
-    safe.take_snapshot([digg, wbtc])
-    trops.take_snapshot([digg, wbtc])
+    tokens = [digg, wbtc, renbtc, yvwbtc, imbtc, fpmbtchbtc]
+    safe.take_snapshot(tokens)
+    trops.take_snapshot(tokens)
 
     stabiliser.governancePullSomeCollateral(wbtc.balanceOf(stabiliser))
     stabiliser.setController(dummy)
     dummy.sweepStratToTrops(stabiliser, {'from': safe.account})
+
+    # do a sweep of all assets from dev to trops
+    digg.transfer(trops, digg.balanceOf(safe))
+    wbtc.transfer(trops, wbtc.balanceOf(safe))
+    renbtc.transfer(trops, renbtc.balanceOf(safe))
+    imbtc.transfer(trops, imbtc.balanceOf(safe))
+    fpmbtchbtc.transfer(trops, fpmbtchbtc.balanceOf(safe))
+    yvwbtc.withdraw(yvwbtc.balanceOf(safe), trops)
+    safe.account.transfer(trops, safe.account.balance())
 
     trops.print_snapshot()
     safe.post_safe_tx()
