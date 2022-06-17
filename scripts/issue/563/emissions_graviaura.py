3ba0a655ccd88727a753e45e4803dd6aad97fe40
Petrovska <petrovska@badger.finance>
2022-06-17 14:01:07 +0200

    Allow setting amounts, starting and ending time via cli

--- scripts/issue/563/emissions_graviaura.py
+++ scripts/issue/563/emissions_graviaura.py
@@ -2,18 +2,20 @@ from great_ape_safe import GreatApeSafe
 from helpers.addresses import registry
 
 
-def main():
+def main(amount=25000000000000000000000, start_ts=1655481600, end_ts=1658505600):
     techops = GreatApeSafe(registry.eth.badger_wallets.techops_multisig)
 
     rewards_logger = techops.contract(registry.eth.rewardsLogger)
 
+    duration = int(end_ts) - int(start_ts)
+
     rewards_logger.setUnlockSchedule(
         registry.eth.sett_vaults.graviAURA,
         registry.eth.treasury_tokens.BADGER,
-        25000000000000000000000,  # 25k badger
-        1655481600,  #  Friday, 17 June 2022 16:00:00 UTC
-        1658505600,  # Friday, 22 July 2022 16:00:00 UTC
-        3024000,
+        int(amount),
+        int(start_ts),
+        int(end_ts),
+        duration,
     )
 
     techops.post_safe_tx()
