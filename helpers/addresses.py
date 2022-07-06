298f0f5cfc4e03eb0a1e123b5aecbcb63e803f51
Jacob Albrecht <33009898+jalbrekt85@users.noreply.github.com>
2022-07-06 12:11:07 -0500

    Update helpers/addresses.py
    Co-authored-by: Gosuto Inzasheru <mail@gosuto.ai>

--- helpers/addresses.py
+++ helpers/addresses.py
@@ -457,7 +457,7 @@ ADDRESSES_ETH = {
         "routerV2": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
         "routerV3": "0xE592427A0AEce92De3Edee1F18E0157C05861564",
         "factoryV2": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-        "quoter": "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
+        "quoter": "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
     },
     "sushiswap": {
         "routerV2": "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
