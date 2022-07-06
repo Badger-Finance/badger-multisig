19e2cef9d3712d720d5a4f3d395c90aca21a4378
jorijnsmit <mail@gosuto.ai>
2022-07-06 18:13:32 +0200

    script: vote to drain the aragon agent wallet of crvrenbtc

--- helpers/addresses.py
+++ helpers/addresses.py
@@ -19,6 +19,7 @@ ADDRESSES_ETH = {
     "GlobalAccessControl": "0x9c58B0D88578cd75154Bdb7C8B013f7157bae35a",
     "harvest_forwarder": "0xA84B663837D94ec41B0f99903f37e1d69af9Ed3E",
     "badger_geyser": "0xBD9c69654B8F3E5978DFd138B00cB0Be29F28cCf",
+    "aragon_voting": "0xDc344bFB12522bF3fa58EF0d6b9a41256fc79A1b",
     "drippers": {
         "rembadger_2022_q2": "0xD87F434fE6d5B349f4376d2daBA762b213E403c7",
         "tree_2022_q2": "0xfA55d407F48849aE98a6e4f11Dc5C18E035F46Ec",
