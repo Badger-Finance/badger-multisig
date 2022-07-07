4f8b7a0d52765e1a9594a0e0fecfee7c1b17ae38
jorijnsmit <mail@gosuto.ai>
2022-07-07 15:06:53 +0200

    fix: include graviaura in claim tree snapshot

--- scripts/badger/claim_tree_rewards.py
+++ scripts/badger/claim_tree_rewards.py
@@ -7,11 +7,12 @@ from helpers.addresses import registry
 def main(msig='treasury_ops_multisig'):
     safe = GreatApeSafe(registry.eth.badger_wallets[msig])
 
-    bcvx = interface.ISettV4h(registry.eth.treasury_tokens.bCVX)
-    bvecvx = interface.ISettV4h(registry.eth.treasury_tokens.bveCVX)
-    bcvxcrv = interface.ISettV4h(registry.eth.treasury_tokens.bcvxCRV)
+    bcvx = safe.contract(registry.eth.treasury_tokens.bCVX)
+    bvecvx = safe.contract(registry.eth.treasury_tokens.bveCVX)
+    bcvxcrv = safe.contract(registry.eth.treasury_tokens.bcvxCRV)
+    gravi = safe.contract(registry.eth.sett_vaults.graviAURA)
 
-    safe.take_snapshot(tokens=[bcvx.address, bvecvx.address, bcvxcrv.address])
+    safe.take_snapshot(tokens=[bcvx, bvecvx, bcvxcrv, gravi])
 
     safe.init_badger()
     safe.badger.claim_all()
