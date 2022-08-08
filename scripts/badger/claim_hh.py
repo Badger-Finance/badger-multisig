79d1cf997fc4909e216f2708963c02b77ddb5024
jalbrekt85 <jcalbrecht85@gmail.com>
2022-08-08 15:18:08 -0500

    send rewards to dest

--- scripts/badger/claim_hh.py
+++ scripts/badger/claim_hh.py
@@ -6,6 +6,7 @@ from helpers.addresses import r
 
 def main(destination="0x042B32Ac6b453485e357938bdC38e0340d4b9276"): # trops
     voter = GreatApeSafe(r.badger_wallets.treasury_voter_multisig)
+    destination = GreatApeSafe(destination)
 
     rewards = voter.contract(r.hidden_hand.rewards_distributor)
 
@@ -13,13 +14,13 @@ def main(destination="0x042B32Ac6b453485e357938bdC38e0340d4b9276"): # trops
     response = requests.get(hh_url)
     data = response.json()["data"]
 
-
     aggregate = {"tokens": [], "amounts": []}
     for item in data:
         aggregate["tokens"].append(item["token"])
         aggregate["amounts"].append(item["claimMetadata"]["amount"])
-    
+
     voter.take_snapshot(tokens=aggregate["tokens"])
+    destination.take_snapshot(tokens=aggregate["tokens"])
 
     rewards.claim(
         [
@@ -33,6 +34,11 @@ def main(destination="0x042B32Ac6b453485e357938bdC38e0340d4b9276"): # trops
             ]
     )
 
-    print(dict(zip(aggregate["tokens"], aggregate["amounts"])))
-
     voter.print_snapshot()
+
+    for token in aggregate["tokens"]:
+        token = voter.contract(token)
+        token.transfer(destination, token.balanceOf(voter))
+
+    destination.print_snapshot()
+    voter.post_safe_tx()
