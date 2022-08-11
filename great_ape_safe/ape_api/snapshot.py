3003bcac4867b58eb29a7f39a4378b205838a201
jalbrekt85 <jcalbrecht85@gmail.com>
2022-08-11 13:32:28 -0500

    output payload & msg hash, small refactor

--- great_ape_safe/ape_api/snapshot.py
+++ great_ape_safe/ape_api/snapshot.py
@@ -42,6 +42,12 @@ class Snapshot():
 
         self.proposal_id = proposal_id
         self.proposal_data = self._get_proposal_data(self.proposal_id)
+    
+    
+    def handle_response(self, response):
+        if not response.ok:
+            console.print(f"Error: {response.text}")
+            raise
 
 
     def _get_proposal_data(self, proposal_id):
@@ -51,10 +57,7 @@ class Snapshot():
             json={"query": self.proposal_query, "variables": {"proposal_id": proposal_id}},
         )
 
-        if not response.ok:
-            print(f"Error getting proposal data: {response.text}")
-            raise
-
+        self.handle_response(response)
         return response.json()["data"]["proposal"]
 
 
@@ -62,23 +65,57 @@ class Snapshot():
         # external helper method to view proposal choices
         choices = self.proposal_data["choices"]
         console.print(f"Choices for proposal {self.proposal_id}: {choices}")
-    
+
+
+    def create_payload_hash(
+            self,
+            payload=None, 
+            timestamp=None, 
+            proposal=None, 
+            choice=None, 
+            version="0.1.3", 
+            type="vote",
+            metadata=""
+        ):
+        # helper method to create message hash from payload and output to console
+        # can be used externally to verify generated hash
+        if not payload:
+            assert all([timestamp, proposal, choice])
+            payload = {
+                "version": version,
+                "timestamp": timestamp,
+                "space": self.proposal_data["space"]["id"],
+                "type": type,
+                "payload": {
+                    "proposal": proposal,
+                    "choice": choice,
+                    "metadata": metadata,
+                },
+            }
+
+        payload_stringify = json.dumps(payload, separators=(",", ":"))
+        hash = messages.defunct_hash_message(text=payload_stringify)
+        console.print(f"msg hash: {hash.hex()}")
+        return hash, payload_stringify
+
 
     def vote_and_post(self, choice, version="0.1.3", type="vote", metadata=""):
-        # given a choice, contruct payload, post to vote relayer and return safe tx
+        # given a choice, contruct payload, post to vote relayer and post safe tx
         # for single vote, pass in choice as str ex: "yes"
         # for weighted vote, pass in choice(s) as dict ex: {"80/20 BADGER/WBTC": 1}
         choices = self.proposal_data["choices"]
         space = self.proposal_data["space"]["id"]
         vote_type = self.proposal_data["type"]
         state = self.proposal_data["state"]
+        is_weighted = vote_type == "weighted"
 
-        assert state == "active", "Vote is not within proposal time window"
+        # assert state == "active", "Vote is not within proposal time window"
         assert isinstance(choice, dict if vote_type == "weighted" else str)
 
-        if vote_type == "weighted":
+        if is_weighted:
+            choices_index = {choices.index(k) + 1: v for k, v in choice.items()}
             choice = json.dumps(
-                {choices.index(k) + 1: v for k, v in choice.items()},
+                choices_index,
                 separators=(",", ":")
             )
         else:
@@ -98,8 +135,15 @@ class Snapshot():
             }
         }
 
-        payload_stringify = json.dumps(payload, separators=(",", ":"))
-        hash = messages.defunct_hash_message(text=payload_stringify)
+        console.print("payload", payload)
+        hash, payload_stringify = self.create_payload_hash(payload)
+
+        if is_weighted:
+            for choice, weight in choices_index.items():
+                console.print(f"signing vote for choice {choice} with {weight * 100}% weight")
+        else:
+            console.print(f"signing vote for choice {choice}")
+
         tx_data = self.sign_message_lib.signMessage.encode_input(hash)
 
         response = requests.post(
@@ -115,9 +159,7 @@ class Snapshot():
             ),
         )
 
-        if not response.ok:
-            print(f"Error notifying relayer: {response.text}")
-            raise
+        self.handle_response(response)
 
         safe_tx = self.safe.build_multisig_tx(
             to=registry.eth.gnosis.sign_message_lib, value=0, data=tx_data, operation=1
