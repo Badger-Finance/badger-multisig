099762f1d1c1327fa9c2d74578c500ad29ecfdb5
jalbrekt85 <jcalbrecht85@gmail.com>
2022-08-29 14:59:18 -0500

    remove `tests/deployment`

--- tests/deployment/gas_station/test_zero_address_error.py
+++
@@ -1,6 +0,0 @@
-import brownie
-
-
-def test_zero_address_error(gas_station):
-    with brownie.reverts('typed error: 0xd92e233d'): # ZeroAddress
-        gas_station.withdraw(gas_station.balance(), brownie.ZERO_ADDRESS)
