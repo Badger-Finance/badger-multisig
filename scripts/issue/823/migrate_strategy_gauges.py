ea1b6fb449d8cc1a0fe37d3fcd14525b2c5378a9
sajanrajdev <sajanrajdev10@gmail.com>
2022-09-21 16:29:00 -0400

    feat: added simulation and checks

--- scripts/issue/823/migrate_strategy_gauges.py
+++ scripts/issue/823/migrate_strategy_gauges.py
@@ -1,7 +1,7 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
 from rich.console import Console
-from brownie import interface
+from brownie import interface, chain, accounts
 
 C = Console()
 
@@ -10,7 +10,12 @@ VAULTS_PIDS = {
     "b40WBTC_40DIGG_20graviAURA": 34,
 }
 
-def main():
+USER = accounts.at("0x53D8EDF6a54239eB785eC72213919Fb6b6B73598", force=True)
+
+BAURABAL = r.sett_vaults.bauraBal
+GRAVIAURA = r.sett_vaults.graviAURA
+
+def main(simulation="false"):
     safe = GreatApeSafe(r.badger_wallets.dev_multisig)
     booster = safe.contract(r.aura.booster)
 
@@ -24,11 +29,17 @@ def main():
         #### 1. Harvest strategy ####
         strat.harvest()
 
+        # There are no more pending rewards
+        pending_rewards = strat.balanceOfRewards()
+        assert pending_rewards[0][1] == 0
+        assert pending_rewards[1][1] == 0
+
         #### 2. Withdraw all to vault ####
         vault_balance_before = want.balanceOf(vault.address)
         pool_balance_before = strat.balanceOfPool()
         strat_balance_before = strat.balanceOfWant()
         total_balance_before = vault.balance()
+        ppfs_before = vault.getPricePerFullShare()
         assert total_balance_before == vault_balance_before + pool_balance_before + strat_balance_before
 
         # Call withdrawToVault
@@ -76,10 +87,35 @@ def main():
         assert available == rewards_d_tkn_balance_after - rewards_d_tkn_balance_before
         # Assert that the total amount of tokens remained the same
         assert total_balance_before == vault.balance()
+        # Ppfs shouldn't change if balance doesn't change but why not
+        assert vault.getPricePerFullShare() == ppfs_before
 
         C.print(f"[green]Migration successful![/green]")
 
-    safe.post_safe_tx()
+        if simulation == "true":
+            # Snapshotting to ensure both simulations run at the same time
+            chain.snapshot()
+            chain.sleep(3*24*3600)
+
+            # Can properly harvest
+            tx = strat.harvest()
+            events = tx.events
+            # Emits bauraBAL and graviAURA
+            assert len(events["TreeDistribution"]) == 2
+            assert events["TreeDistribution"][0]["token"] == BAURABAL
+            assert events["TreeDistribution"][0]["amount"] > 0
+            assert events["TreeDistribution"][1]["token"] == GRAVIAURA
+            assert events["TreeDistribution"][1]["amount"] > 0
+
+            # Can properly withdraw
+            user_balance_before = want.balanceOf(USER)
+            vault.withdrawAll({"from": USER})
+            assert want.balanceOf(USER) > user_balance_before
+
+            chain.revert()
+
+    if simulation == "false":
+        safe.post_safe_tx()
 
 
 
