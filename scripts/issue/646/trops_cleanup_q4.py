04d79a36189b5686fe0cc47791917b46a16b8a64
jalbrekt85 <jcalbrecht85@gmail.com>
2022-09-30 10:42:53 -0500

    deposit for voter and tree, add bribe tokens

--- scripts/issue/646/trops_cleanup_q4.py
+++ scripts/issue/646/trops_cleanup_q4.py
@@ -1,3 +1,5 @@
+from brownie import interface
+
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
 
@@ -5,6 +7,8 @@ from helpers.addresses import r
 def main():
     trops = GreatApeSafe(r.badger_wallets.treasury_ops_multisig)
     vault = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
+    voter = GreatApeSafe(r.badger_wallets.treasury_voter_multisig)
+    tree = GreatApeSafe(r.badger_wallets.badgertree)
     trops.init_badger()
     trops.init_curve()
 
@@ -17,12 +21,12 @@ def main():
     usdc = trops.contract(r.treasury_tokens.USDC)
     baurabal = trops.contract(r.sett_vaults.bauraBal)
 
-    send_to_vault = [cvxcrv, bcvxcrv, baurabal]
-
     trops.take_snapshot(
-        tokens=[bvecvx, bcvxcrv, gravi, usdc, baurabal, cvx] + send_to_vault
+        tokens=[bvecvx, cvxcrv, gravi, usdc, baurabal, cvx, baurabal]
     )
-    vault.take_snapshot(tokens=send_to_vault)
+    vault.take_snapshot(tokens=[baurabal])
+    voter.take_snapshot(tokens=[bvecvx])
+    tree.take_snapshot(tokens=[bcvxcrv])
 
     # receive bvecvx, bcvxCRV, graviAURA
     trops.badger.claim_all()
@@ -30,16 +34,22 @@ def main():
     # withdraw all 3crv to usdc
     trops.curve.withdraw_to_one_coin(crv3pool, crv3pool.balanceOf(trops), usdc)
 
-    # deposit cvx
+    # deposit cvx for voter
     cvx.approve(bvecvx, cvx.balanceOf(trops))
-    bvecvx.deposit(cvx.balanceOf(trops))
+    bvecvx.depositFor(voter, cvx.balanceOf(trops))
+
+    # deposit cvxcrv for tree
+    cvxcrv.approve(bcvxcrv, cvxcrv.balanceOf(trops))
+    bcvxcrv.depositFor(tree, cvxcrv.balanceOf(trops))
+    bcvxcrv.transfer(tree, bcvxcrv.balanceOf(trops))
 
     # transfers to vault
-    for token in send_to_vault:
-        token.transfer(vault, token.balanceOf(trops))
+    baurabal.transfer(vault, baurabal.balanceOf(trops))
 
     trops.print_snapshot()
     vault.print_snapshot()
+    voter.print_snapshot()
+    tree.print_snapshot()
     trops.post_safe_tx()
 
 
@@ -48,12 +58,14 @@ def sell_to_weth():
     trops.init_cow(prod=False)
 
     sd = trops.contract(r.bribe_tokens_claimable_graviaura.SD)
+    dfx = trops.contract(r.bribe_tokens_claimable_graviaura.DFX)
+    rpl = trops.contract(r.bribe_tokens_claimable_graviaura.RPL)
     ens = trops.contract(r.treasury_tokens.ENS)
     sushi = trops.contract(r.treasury_tokens.SUSHI)
     xsushi = trops.contract(r.treasury_tokens.xSUSHI)
     weth = trops.contract(r.treasury_tokens.WETH)
 
-    for token in [sd, ens, sushi, xsushi]:
+    for token in [sd, ens, dfx, rpl, sushi, xsushi]:
         trops.cow.market_sell(token, weth, token.balanceOf(trops), deadline=60 * 60 * 4)
 
     trops.post_safe_tx()
