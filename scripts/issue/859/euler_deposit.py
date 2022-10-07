4fadd7ae94effb21c69b4b0d09c6b478f8af5646
jalbrekt85 <jcalbrecht85@gmail.com>
2022-10-07 14:25:23 -0500

    add ebadger to snapshot

--- scripts/issue/859/euler_deposit.py
+++ scripts/issue/859/euler_deposit.py
@@ -11,22 +11,13 @@ def main():
     vault.init_euler()
     
     badger = vault.contract(r.treasury_tokens.BADGER)
-    ebadger = interface.IERC20(
+    ebadger = interface.IEToken(
             vault.euler.markets.underlyingToEToken(badger)
         )
 
-    before_ebadger = ebadger.balanceOf(vault) / 1e18
-
-    vault.take_snapshot(tokens=[badger])
+    vault.take_snapshot(tokens=[badger, ebadger])
 
     vault.euler.deposit(badger, 100_000e18)
-
-    after_ebadger = ebadger.balanceOf(vault)/ 1e18
-    delta = after_ebadger - before_ebadger
-
-    console.print(f"{before_ebadger=}")
-    console.print(f"{after_ebadger=}")
-    console.print(f"{delta=}")
 
     vault.print_snapshot()
     vault.post_safe_tx()
