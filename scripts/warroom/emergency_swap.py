5cbec3e68e221ea1f55605fd795125841dde8b2a
jalbrekt85 <jcalbrecht85@gmail.com>
2022-11-23 13:55:50 -0600

    change use_3pool to be read from cli

--- scripts/warroom/emergency_swap.py
+++ scripts/warroom/emergency_swap.py
@@ -6,27 +6,27 @@ from helpers.addresses import r
 console = Console()
 
 
-def main(symbol_in, symbol_out, amount=None, use_3pool=True):
+def main(symbol_in, symbol_out, amount=None, use_3pool="true"):
     vault = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
 
     tokens = {
         "usdc": vault.contract(r.treasury_tokens.USDC),
         "usdt": vault.contract(r.treasury_tokens.USDT),
-        "dai": vault.contract(r.treasury_tokens.DAI)
+        "dai": vault.contract(r.treasury_tokens.DAI),
     }
-    
+
     token_in = tokens[symbol_in]
     token_out = tokens[symbol_out]
     amount = token_in.balanceOf(vault) if not amount else int(amount)
 
     vault.take_snapshot(tokens=[token_in, token_out])
 
-    if use_3pool:
-        vault.init_curve()
+    if use_3pool == "true":
+        vault.init_curve(prod=False)
         vault.curve.swap(token_in, token_out, amount)
-    
+
     else:
         vault.init_cow()
-        vault.cow.market_sell(token_in, token_out, amount, 60 * 60 *4)
+        vault.cow.market_sell(token_in, token_out, amount, 60 * 60 * 4)
 
     vault.print_snapshot()
