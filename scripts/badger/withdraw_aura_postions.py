4a02f3178807c7332461b8ca980a232394928a6a
sajanrajdev <sajanrajdev10@gmail.com>
2022-12-10 13:39:11 -0800

    feat: add rewards contracts

--- scripts/badger/withdraw_aura_postions.py
+++ scripts/badger/withdraw_aura_postions.py
@@ -1,12 +1,18 @@
 from rich.console import Console
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
+from brownie import Contract
 
 console = Console()
 
 BADGER80WBTC20 = r.balancer.B_20_BTC_80_BADGER
 BADGER50RETH50 = r.balancer.B_50_BADGER_50_RETH
 WBTC40DIGG40GRAVI20 = r.balancer.bpt_40wbtc_40digg_20graviaura
+
+BADGER80WBTC20_AURA = r.aura.aura_20wbtc80badger
+BADGER50RETH50_AURA = r.aura.aura_50reth50badger
+WBTC40DIGG40GRAVI20_AURA = r.aura.aura_40wbtc40digg20gravi
+
 
 def main():
     vault = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
@@ -19,16 +25,32 @@ def main():
     badgerreth_bpt = vault.contract(BADGER50RETH50)
     wbtcdigggrav_bpt = vault.contract(WBTC40DIGG40GRAVI20)
 
-    # snaps
+    badgerwbtc_rewards = Contract.from_explorer(BADGER80WBTC20_AURA, owner=vault.account)
+    badgerreth_rewards = Contract.from_explorer(BADGER50RETH50_AURA, owner=vault.account)
+    wbtcdigggrav_rewards = Contract.from_explorer(WBTC40DIGG40GRAVI20_AURA, owner=vault.account)
+
+    # # snaps
     # tokens = [bal, aura, auraBAL]
     # vault.take_snapshot(tokens)
 
-    # Claim all accrued rewards
+    # # Claim all accrued rewards
     # vault.aura.claim_all_from_booster()
 
     # Withdraw all from positions
-    vault.aura.unstake_all_and_withdraw_all(badgerwbtc_bpt)
-    vault.aura.unstake_all_and_withdraw_all(badgerreth_bpt)
-    vault.aura.unstake_all_and_withdraw_all(wbtcdigggrav_bpt)
+    badgerwbtc_bpt_bal = badgerwbtc_bpt.balanceOf(vault)
+    badgerreth_bpt_bal = badgerreth_bpt.balanceOf(vault)
+    wbtcdigggrav_bpt_bal = wbtcdigggrav_bpt.balanceOf(vault)
+
+    badgerwbtc_bpt_staked_bal = badgerwbtc_rewards.balanceOf(vault)
+    badgerreth_bpt_staked_bal = badgerreth_rewards.balanceOf(vault)
+    wbtcdigggrav_bpt_staked_bal = wbtcdigggrav_rewards.balanceOf(vault)
+
+    badgerwbtc_rewards.withdrawAllAndUnwrap(True)
+    badgerreth_rewards.withdrawAllAndUnwrap(True)
+    wbtcdigggrav_rewards.withdrawAllAndUnWrap(True)
+
+    assert badgerwbtc_bpt_staked_bal == badgerwbtc_bpt.balanceOf(vault) - badgerwbtc_bpt_bal
+    assert badgerreth_bpt_staked_bal == badgerreth_bpt.balanceOf(vault) - badgerreth_bpt_bal
+    assert wbtcdigggrav_bpt_staked_bal == wbtcdigggrav_bpt.balanceOf(vault) - wbtcdigggrav_bpt_bal
 
     vault.post_safe_tx(gen_tenderly=False)
