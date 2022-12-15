4625577657ad3a2f986ea426197d2650385c21d6
jalbrekt85 <jcalbrecht85@gmail.com>
2022-12-15 10:33:33 -0600

    stake work

---
+++ scripts/issue/1021/stake_work.py
@@ -0,0 +1,16 @@
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import r
+
+
+def main():
+    safe = GreatApeSafe(r.badger_wallets.techops_multisig)
+    safe.init_opolis()
+    
+    work = safe.contract(r.coingecko_tokens.WORK)
+
+    safe.take_snapshot(tokens=[work])
+
+    safe.opolis.stake(safe.balanceOf(work))
+
+    safe.print_snapshot()
+    safe.post_safe_tx()
