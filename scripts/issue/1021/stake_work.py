5b654c0b6f694be7359c14f2a029ecd8eca31ea6
jalbrekt85 <jcalbrecht85@gmail.com>
2022-12-15 10:34:39 -0600

    linting

--- scripts/issue/1021/stake_work.py
+++ scripts/issue/1021/stake_work.py
@@ -5,7 +5,7 @@ from helpers.addresses import r
 def main():
     safe = GreatApeSafe(r.badger_wallets.techops_multisig)
     safe.init_opolis()
-    
+
     work = safe.contract(r.coingecko_tokens.WORK)
 
     safe.take_snapshot(tokens=[work])
