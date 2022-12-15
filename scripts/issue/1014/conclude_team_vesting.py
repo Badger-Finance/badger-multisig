d4e4792ebec1afa91423d41f350eef31fe944d80
jalbrekt85 <jcalbrecht85@gmail.com>
2022-12-15 11:21:34 -0600

    seperate test tx, add balance checker

--- scripts/issue/1014/conclude_team_vesting.py
+++ scripts/issue/1014/conclude_team_vesting.py
@@ -1,5 +1,6 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
+from brownie import interface
 
 
 TO_DISTRIBUTE = 728820123211567732115678 - 2e18
@@ -11,15 +12,30 @@ recipient_amounts = {
 
 safe = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
 badger = safe.contract(r.treasury_tokens.BADGER)
+balance_checker = interface.IBalanceChecker(
+    r.helpers.balance_checker, owner=safe.account
+)
 
 
 def main():
     safe.take_snapshot(tokens=[badger])
 
     for recipient, amount in recipient_amounts.items():
-        # test transfer of 1 BADGER first
-        badger.transfer(recipient, 1e18)
+        recipient_bal_before = badger.balanceOf(recipient)
         badger.transfer(recipient, amount)
+        balance_checker.verifyBalance(badger, recipient, recipient_bal_before + amount)
+
+    safe.print_snapshot()
+    safe.post_safe_tx()
+
+
+def test_transfer():
+    safe.take_snapshot(tokens=[badger])
+
+    for recipient in recipient_amounts.keys():
+        recipient_bal_before = badger.balanceOf(recipient)
+        badger.transfer(recipient, 1e18)
+        balance_checker.verifyBalance(badger, recipient, recipient_bal_before + 1e18)
 
     safe.print_snapshot()
     safe.post_safe_tx()
