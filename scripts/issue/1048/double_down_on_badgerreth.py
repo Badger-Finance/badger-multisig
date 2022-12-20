46e7e16ff82d1e2daf82d59a1fa31ceaf2261e32
gosuto-inzasheru <mail@gosuto.ai>
2022-12-20 14:25:37 +0530

    feat: swap weth to reth and deposit into badgerreth

---
+++ scripts/issue/1048/double_down_on_badgerreth.py
@@ -0,0 +1,69 @@
+from brownie import ETH_ADDRESS
+from web3 import Web3
+
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import r
+
+
+AMNT_BADGER = 491_000e18
+AMNT_ETH = 932.96e18
+SLIPPAGE = 0.98
+
+
+def main():
+    treasury = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
+    trops = GreatApeSafe(r.badger_wallets.treasury_ops_multisig)
+    reth = treasury.contract(r.treasury_tokens.RETH)
+    weth = treasury.contract(r.treasury_tokens.WETH)
+    badger = treasury.contract(r.treasury_tokens.BADGER)
+    bpt_badgerreth = treasury.contract(r.balancer.B_50_BADGER_50_RETH)
+    bpt_wethreth = treasury.contract(r.balancer.B_50_WETH_50_RETH)
+    rocket_deposit = treasury.contract(r.rocket_pool.deposit)
+    rocket_storage = treasury.contract(r.rocket_pool.storage)
+    rocket_vault = treasury.contract(
+        rocket_storage.getAddress(
+            Web3.solidityKeccak(["bytes32"], [b"contract.address" + b"rocketVault"])
+        )
+    )
+    rocket_settings = treasury.contract(
+        rocket_storage.getAddress(
+            Web3.solidityKeccak(
+                ["bytes32"], [b"contract.address" + b"rocketDAOProtocolSettingsDeposit"]
+            )
+        )
+    )
+
+    trops.take_snapshot([weth])
+    treasury.take_snapshot([reth, weth, badger, bpt_badgerreth])
+    treasury.init_balancer()
+
+    reth_needed = AMNT_ETH - (reth.balanceOf(treasury) * reth.getExchangeRate() / 1e18)
+
+    # how much reth can we get natively?
+    # ref: https://github.com/rocket-pool/rocketpool/blob/93f794b8d4aabe4c3e3c64170c475d337bdb9d28/contracts/contract/deposit/RocketDepositPool.sol#L80
+    reth_natively = (
+        rocket_settings.getMaximumDepositPoolSize()
+        - rocket_vault.balanceOf(b"rocketDepositPool")
+    )
+    if reth_natively >= reth_needed:
+        weth.withdraw(reth_natively)
+        rocket_deposit.deposit({"value": reth_natively})
+        swap_out = reth_needed - reth_natively
+    else:
+        swap_out = reth_needed
+
+    treasury.balancer.swap(weth, reth, swap_out, pool=bpt_wethreth)
+
+    treasury.balancer.deposit_and_stake(
+        [badger, reth],
+        [AMNT_BADGER, reth.balanceOf(treasury) * SLIPPAGE],
+        pool=bpt_badgerreth,
+        stake=False,
+    )
+
+    # send remaining weth to trops for gas operations
+    weth.transfer(trops, weth.balanceOf(treasury))
+
+    trops.print_snapshot()
+
+    treasury.post_safe_tx()
