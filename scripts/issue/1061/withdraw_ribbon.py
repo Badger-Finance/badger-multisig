10c6f08b8021a34979fc91b062ceba06dfc8d62f
gosuto-inzasheru <mail@gosuto.ai>
2023-01-03 12:37:35 +0200

    feat: call to finalise withdrawal process

--- scripts/issue/1061/withdraw_ribbon.py
+++ scripts/issue/1061/withdraw_ribbon.py
@@ -2,7 +2,14 @@ from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
 
 
-def main():
+def exec_wd():
+    vault = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
+    ribbon = vault.contract(r.ribbon.badger_vault)
+    ribbon.completeWithdraw()
+    vault.post_safe_tx()
+
+
+def initiate_wd():
     vault = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
     ribbon = vault.contract(r.ribbon.badger_vault)
     ribbon.initiateWithdraw(
