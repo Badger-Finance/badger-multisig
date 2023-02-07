454eb50928dcf0b741c2a9d0a6448935091390a8
jalbrekt85 <jcalbrecht85@gmail.com>
2023-02-07 13:03:36 -0600

    linting

--- great_ape_safe/ape_api/uni_v3.py
+++ great_ape_safe/ape_api/uni_v3.py
@@ -45,7 +45,7 @@ class UniV3:
         )
 
         # constant helpers
-        self.Q128 = 2 ** 128
+        self.Q128 = 2**128
         self.deadline = 60 * 180
         self.slippage = 0.98
 
@@ -333,8 +333,8 @@ class UniV3:
         decimals_diff = token1.decimals() - token0.decimals()
 
         # params for minting method
-        lower_tick = int(math.log((1 / range1) * 10 ** decimals_diff, BASE) // 60 * 60)
-        upper_tick = int(math.log((1 / range0) * 10 ** decimals_diff, BASE) // 60 * 60)
+        lower_tick = int(math.log((1 / range1) * 10**decimals_diff, BASE) // 60 * 60)
+        upper_tick = int(math.log((1 / range0) * 10**decimals_diff, BASE) // 60 * 60)
         deadline = chain.time() + self.deadline
 
         # calcs for min amounts
@@ -460,10 +460,9 @@ class UniV3:
         multihop_path = self._build_multihop_path(path)
         path_encoded = self._encode_path(multihop_path)
 
-        min_out = self.quoter.quoteExactInput.call(
-            path_encoded,
-            mantissa,
-        ) * (1 - self.slippage)
+        min_out = self.quoter.quoteExactInput.call(path_encoded, mantissa) * (
+            1 - self.slippage
+        )
 
         params = (
             path_encoded,
