4d52798ffae43071294ed10b16f28a0e0e8f5161
Petrovska <petrovska@badger.com>
2023-02-15 23:24:59 +0800

    feat: reorg script based on feedback

--- scripts/issue/1133/upkeep_manager_integration.py
+++ scripts/issue/1133/upkeep_manager_integration.py
@@ -1,4 +1,4 @@
-from brownie import interface
+from brownie import chain
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
 
@@ -21,23 +21,28 @@ upkeep_manager_snap = GreatApeSafe(r.badger_wallets.upkeep_manager)
 
 # contract
 upkeep_manager = techops.contract(r.badger_wallets.upkeep_manager)
+INITIAL_ROUNDS_TOP_UP = 20
+
+techops.init_badger()
+techops.init_chainlink()
 
 
-def initial_setup():
+def sim():
+    initial_setup_and_testing(sim=True)
+    cancel_dummy_member(sim=True)
+    cancel_upkeeps(sim=True)
+    chain.mine(51)
+    register_members_in_manager(sim=True)
+
+
+def initial_setup_and_testing(sim=False):
     """
-    carries the following actions: cancels upkeeps from both registries,
-    sends techops link funds to manager, init the base upkeep in the manager,
-    updates watchlist, updates registry used in the gas station to latest and
-    register the gas station so it sends 1 eth to the manager
+    carries the following actions: sends techops link funds to manager,
+    init the base upkeep in the manager and adds a dummy member for testing
     """
-    techops.init_badger()
-    techops.init_chainlink()
-
     # snaps
     techops.take_snapshot([techops.chainlink.link])
     upkeep_manager_snap.take_snapshot([techops.chainlink.link])
-
-    initial_rounds_top_up = upkeep_manager.roundsTopUp()
 
     # 1. Send links funds to allow to init base upkeep job
     link_bal = techops.chainlink.link.balanceOf(techops)
@@ -51,38 +56,51 @@ def initial_setup():
     # ref of gas snap: https://github.com/Badger-Finance/badger-avatars/blob/main/.gas-snapshot#L166-L176
     upkeep_manager.initializeBaseUpkeep(1_000_000)
 
-    upkeep_manager_snap.print_snapshot()
+    upkeep_manager.addMember(r.drippers.tree_2022_q2, "TreeDripper2022Q2", 400_000, 0)
 
-    # 2. Cancel upkeep in old and new registry to migrate ownership to the upkeep manager
+    if not sim:
+        upkeep_manager_snap.print_snapshot()
+        techops.post_safe_tx(call_trace=True)
+
+
+def cancel_upkeeps(sim=False):
+    """
+    carries the following actions: cancels upkeeps from both registries,
+    updates watchlist and updates registry used in the gas station to latest
+    """
+    # 1. Cancel upkeep in old and new registry to migrate ownership to the upkeep manager
     for name, upkeep_id in members.items():
         if name == "GasStationExact":
             techops.chainlink.keeper_registry_v1_1.cancelUpkeep(upkeep_id)
         else:
             techops.chainlink.keeper_registry.cancelUpkeep(upkeep_id)
 
-    # 3. Update gas station
+    # 2. Update gas station
     techops.badger.set_gas_station_watchlist()
 
-    # 4. Set `s_keeperRegistryAddress` to latest cl registry
+    # 3. Set `s_keeperRegistryAddress` to latest cl registry
     techops.badger.station.setKeeperRegistryAddress(techops.chainlink.keeper_registry)
 
-    # 5. Add gas st in this tx with the left-over link funds to auto-fund the manager in next block with eth
-    upkeep_manager.addMember(techops.badger.station, "GasStationExact", 400_000, 0)
-    upkeep_manager.setRoundsTopUp(initial_rounds_top_up)
-
-    upkeep_manager_snap.print_snapshot()
-    techops.post_safe_tx(call_trace=True)
+    if not sim:
+        techops.post_safe_tx(call_trace=True)
 
 
-def register_members_in_manager():
-    techops.init_chainlink()
-
+def register_members_in_manager(sim=False):
+    """
+    registers all members in the upkeep manager reading the
+    target address and gas limit from old and latest cl registry
+    """
     for name, upkeep_id in members.items():
         if name == "GasStationExact":
+            (
+                member_address,
+                gas_limit,
+            ) = techops.chainlink.keeper_registry_v1_1.getUpkeep(upkeep_id)[0:2]
             # NOTE: moving forward makes more sense to have links funds in upkeep manager
             techops.chainlink.keeper_registry_v1_1.withdrawFunds(
                 upkeep_id, upkeep_manager
             )
+            upkeep_manager.addMember(member_address, name, gas_limit, 0)
         else:
             member_address, gas_limit = techops.chainlink.keeper_registry.getUpkeep(
                 upkeep_id
@@ -91,23 +109,19 @@ def register_members_in_manager():
             # https://etherscan.io/address/0x4c02f0160dc0387b13bcb5e1728c780649e109ac#code#F16#L166
             upkeep_manager.addMember(member_address, name, gas_limit, 0)
 
-    techops.post_safe_tx()
+    upkeep_manager.setRoundsTopUp(INITIAL_ROUNDS_TOP_UP)
+
+    if not sim:
+        techops.post_safe_tx(call_trace=True)
+    else:
+        techops.post_safe_tx(skip_preview=True)
 
 
-def register_dummy_member():
-    """
-    registers an old dripper to test the flow of registration in the cl registry
-    and later being able to cancel
-    """
-    upkeep_manager.addMember(r.drippers.tree_2022_q2, "TreeDripper2022Q2", 400_000, 0)
-
-    techops.post_safe_tx()
-
-
-def cancel_dummy_member():
+def cancel_dummy_member(sim=False):
     """
     cancels the old dripper
     """
     upkeep_manager.cancelMemberUpkeep(r.drippers.tree_2022_q2)
 
-    techops.post_safe_tx()
+    if not sim:
+        techops.post_safe_tx()
