db335fc3a05dc9d57c2f7fae1e8239540618a45f
gosuto-inzasheru <mail@gosuto.ai>
2023-03-22 16:30:22 -0700

    chore: no weth available so use ether

--- scripts/issue/368/mainnet_avoid_gnosis_api.py
+++ scripts/issue/368/mainnet_avoid_gnosis_api.py
@@ -5,9 +5,12 @@ from helpers.addresses import registry
 
 
 def main():
-    safe = GreatApeSafe(registry.eth.badger_wallets.test_multisig)
-    weth = interface.ERC20(registry.eth.treasury_tokens.WETH, owner=safe.account)
-    weth.transfer(safe, 0.0001e18)
-    weth.transfer(safe, 0.0001e18)
+    safe = GreatApeSafe(registry.eth.badger_wallets.test_multisig_v1_3)
+    # weth = interface.ERC20(registry.eth.treasury_tokens.WETH, owner=safe.account)
+    # weth.transfer(safe, 0.0001e18)
+    # weth.transfer(safe, 0.0001e18)
+    safe.account.transfer(safe.account, 1)
+    safe.account.transfer(safe.account, 2)
+    safe.account.transfer(safe.account, 3)
 
     safe.post_safe_tx_manually()
