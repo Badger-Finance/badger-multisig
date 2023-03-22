8acfe1b0e1d437fa7084435e50e72dcf67d20147
gosuto-inzasheru <mail@gosuto.ai>
2023-03-21 17:09:25 -0700

    style: linting

--- scripts/issue/368/mainnet_avoid_gnosis_api.py
+++ scripts/issue/368/mainnet_avoid_gnosis_api.py
@@ -6,10 +6,8 @@ from helpers.addresses import registry
 
 def main():
     safe = GreatApeSafe(registry.eth.badger_wallets.test_multisig)
-    weth = interface.ERC20(
-        registry.eth.treasury_tokens.WETH, owner=safe.account
-    )
-    weth.transfer(safe, .0001e18)
-    weth.transfer(safe, .0001e18)
+    weth = interface.ERC20(registry.eth.treasury_tokens.WETH, owner=safe.account)
+    weth.transfer(safe, 0.0001e18)
+    weth.transfer(safe, 0.0001e18)
 
     safe.post_safe_tx_manually()
--- scripts/issue/368/rinkeby_avoid_gnosis_api.py
+++ scripts/issue/368/rinkeby_avoid_gnosis_api.py
@@ -6,9 +6,7 @@ from helpers.addresses import registry
 
 def main():
     safe = GreatApeSafe(registry.rinkeby.badger_wallets.rinkeby_multisig)
-    dai = interface.ERC20(
-        registry.rinkeby.treasury_tokens.DAI, owner=safe.account
-    )
+    dai = interface.ERC20(registry.rinkeby.treasury_tokens.DAI, owner=safe.account)
     dai.transfer(safe, 1_000_000e18)
     dai.transfer(safe, 2_000_000e18)
 
