4bbbfac7afdec6ccbdb5a458f252a73f842c0648
Petrovska <petrovska@badger.com>
2023-05-31 12:55:45 -0600

    feat: upgrade ownership of mint/burn controller to timelock

---
+++ scripts/issue/1277/mint_controller_admin_update.py
@@ -0,0 +1,12 @@
+from great_ape_safe import GreatApeSafe
+from helpers.addresses import r
+
+def main():
+    safe = GreatApeSafe(r.badger_wallets.dev_multisig)
+
+    # contracts
+    controller = safe.contract(r.controllers.minting)
+
+    controller.transferOwnership(r.governance_timelock)
+
+    safe.post_safe_tx()
