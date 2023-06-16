96e24a3ba5d809820617a5d30181ad1ea57eaa72
sajanrajdev <sajanrajdev10@gmail.com>
2023-06-16 16:45:23 +0200

    feat: add batching

--- scripts/warroom/pause_deposits.py
+++ scripts/warroom/pause_deposits.py
@@ -1,14 +1,17 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
 
+# Add the vaults' addresses that you wish to pause deposits on
+VAULTS_ARRAY = []
 
-def main(vault_address):
+def main():
     """
     Pauses deposits on a given Vault V1.5 governed by the Dev Multisig
     """
     dev = GreatApeSafe(r.badger_wallets.dev_multisig)
     dev.init_badger()
 
-    dev.badger.pause_deposits(vault_address)
+    for vault_address in VAULTS_ARRAY:
+        dev.badger.pause_deposits(vault_address)
 
     dev.post_safe_tx()
