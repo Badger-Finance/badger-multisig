0057a44d4015419ae2104651771e850116a51b1b
sajanrajdev <sajanrajdev10@gmail.com>
2024-01-24 16:26:51 -0700

    feat: added sim, assertions and snapshots

--- scripts/issue/1489/assoc_and_treasury_council_funding.py
+++ scripts/issue/1489/assoc_and_treasury_council_funding.py
@@ -1,8 +1,9 @@
 from great_ape_safe import GreatApeSafe
 from helpers.addresses import r
+from brownie import accounts
 
 # flag
-PROD = True
+PROD = False
 COEF = 0.99
 DEADLINE = 60 * 60 * 4
 
@@ -13,6 +14,9 @@ COUNCILLOR_PAYMENT_COVERED = 2927.777777777777784336e6
 # funding figure (association)
 ASSOC_Q1_FUNDING_STABLES = 1_154_918
 ASSOC_Q1_FUNDING_BADGER = 137_706e18
+
+# testing
+USDC_WHALE = "0xD6153F5af5679a75cC85D8974463545181f48772"
 
 
 def sell_stable_for_usdc():
@@ -45,26 +49,39 @@ def sell_stable_for_usdc():
     vault.post_safe_tx()
 
 
-def assoc_and_council_funding():
+def assoc_and_council_funding(sim=False):
     vault = GreatApeSafe(r.badger_wallets.treasury_vault_multisig)
+    payments = GreatApeSafe(r.badger_wallets.payments_multisig_2024)
 
     # tokens
     usdc = vault.contract(r.treasury_tokens.USDC)
     badger = vault.contract(r.treasury_tokens.BADGER)
+
+    # for testing purposes (USDC acquisition)
+    if sim:
+        usdc_whale = accounts.at(USDC_WHALE, force=True)
+        usdc.transfer(vault.account, 2_000_000e6, {"from": usdc_whale})
+
+    vault.take_snapshot([usdc, badger])
+    payments.take_snapshot([usdc, badger])
 
     # contracts
     llamapay = vault.contract(r.llamapay)
 
     # 1. transfer funding to assoc. wallet
     usdc.transfer(
-        r.badger_wallets.payments_multisig_2024,
+        payments.account,
         ASSOC_Q1_FUNDING_STABLES * 10 ** usdc.decimals(),
     )
-    badger.transfer(r.badger_wallets.payments_multisig_2024, ASSOC_Q1_FUNDING_BADGER)
+    badger.transfer(payments.account, ASSOC_Q1_FUNDING_BADGER)
 
     # 2. top-up yearly funding for treasury council
     usdc.approve(llamapay, TREASURY_COUNCIL_YEARLY_FUNDING)
     llamapay.deposit(TREASURY_COUNCIL_YEARLY_FUNDING)
+    # Accounting on LlamaPay is done in 1e20 units (https://etherscan.io/address/0x3e67cc2c7fff86d9870db9d02c43e789b52fb296#code#F1#L218)
+    assert llamapay.balances(
+        vault.account
+    ) / 1e20 == TREASURY_COUNCIL_YEARLY_FUNDING / (10 ** usdc.decimals())
 
     # 3. transfer $usdc to councillors members for covering payment before stream initialisation this month
     usdc.transfer(
@@ -83,4 +100,6 @@ def assoc_and_council_funding():
         r.badger_wallets.treasury_councillors.councillor5, COUNCILLOR_PAYMENT_COVERED
     )
 
+    payments.print_snapshot()
+    vault.print_snapshot()
     vault.post_safe_tx()
